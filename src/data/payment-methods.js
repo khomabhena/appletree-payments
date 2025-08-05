3fef08e738f37ca680e9eab4445a7349a12fd34d
kho-appleseed <kholwani@appleseed.tech>
2025-08-05 10:07:48 +0200

    Added more payment methods

--- src/data/payment-methods.js
+++ src/data/payment-methods.js
@@ -1,34 +1,102 @@
 const paymentMethodsOffered = [
     {
-        image: '/payment-methods/01-pm.jpg',
+        image: '/payment-methods/visa.png',
         title: 'Innbucks',
     },
     {
-        image: '/payment-methods/02-pm.jpg',
+        image: '/payment-methods/mastercard.png',
         title: 'PayPal',
     },
     {
-        image: '/payment-methods/03-pm.jpg',
+        image: '/payment-methods/ecocash.png',
         title: 'Orange',
     },
     {
-        image: '/payment-methods/04-pm.jpg',
+        image: '/payment-methods/one-money2.png',
         title: 'Vodapay',
     },
     {
-        image: '/payment-methods/05-pm.jpg',
+        image: '/payment-methods/o-mari.png',
         title: 'tigopesa',
     },
     {
-        image: '/payment-methods/06-pm.jpg',
+        image: '/payment-methods/zipit.png',
         title: 'EcoCash',
     },
     {
-        image: '/payment-methods/07-pm.jpg',
+        image: '/payment-methods/zim-switch.png',
         title: 'Airtel',
     },
     {
-        image: '/payment-methods/08-pm.jpg',
+        image: '/payment-methods/inn-bucks.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/telecash.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/american-express.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/mtn.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/alipay.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/vodapay.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/wechat-pay.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/tigo-pesa.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/airtel.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/union-pay.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/express-union.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/halo-pesa.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/movitel.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/m-pesa.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/tigo-pesa.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/payshap.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/t-money.png',
+        title: 'MPesa',
+    },
+    {
+        image: '/payment-methods/ozow.png',
         title: 'MPesa',
     },
 ]

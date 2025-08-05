5f69fea7e6bc0a4f0970a9dea97fe87030ce7164
khomabhena <colwanymab@gmail.com>
2025-08-05 10:24:24 +0200

    changed navigation width

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
    const textColor = colors.primaryColor
 
   return (
-    <nav className=' fixed top-0 left-0 right-0 bg-black flex justify-between items-center px-4 h-18'>
+    <nav className=' fixed top-0 left-0 right-0 bg-black max-w-[1920px] flex justify-between items-center px-4 h-18'>
       <img className=' cursor-pointer h-14 md:h-18 mt-4 md:mt-8 w-auto' src="/appletree-payments-logo-white.png" alt="White Appletree logo" />
 
       <ul className=' hidden lg:flex gap-8 text-base pr-4' style={{ color: textColor }}>

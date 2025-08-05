e1f7e1347bfc5e4c448d0bb7053c66cc3257bac1
khomabhena <colwanymab@gmail.com>
2025-08-05 12:44:21 +0200

    added smooth scrolling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,9 @@ const Navigation = () => {
 
   return (
     <nav className=' fixed top-0 left-0 right-0 bg-black m-auto max-w-[1920px] flex justify-between items-center px-4 h-18'>
-      <img className=' cursor-pointer h-14 md:h-18 mt-4 md:mt-8 w-auto' src="/appletree-payments-logo-white.png" alt="White Appletree logo" />
+      <a href="#hero">
+        <img className=' cursor-pointer h-14 md:h-18 mt-4 md:mt-8 w-auto' src="/appletree-payments-logo-white.png"  alt="White Appletree logo" />
+      </a>
 
       <ul className=' hidden lg:flex gap-8 text-base pr-4' style={{ color: textColor }}>
         <li className=' hover:text-green-800 hover:scale-105'><a href="#payment-solutions">Payment Solutions</a></li>

45f302715050d119fd02bba77502e2bd7a6791e5
khomabhena <colwanymab@gmail.com>
2025-08-05 14:51:59 +0200

    tried modifying the payment methods but didn't manage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
    const textColor = colors.primaryColor
 
   return (
-    <nav className=' fixed top-0 left-0 right-0 bg-black m-auto max-w-[1920px] flex justify-between items-center px-4 h-18'>
+    <nav className=' fixed top-0 left-0 right-0 bg-black m-auto max-w-[1920px] z-20 flex justify-between items-center px-4 h-18'>
       <a href="#hero">
         <img className=' cursor-pointer h-14 md:h-18 mt-4 md:mt-8 w-auto' src="/appletree-payments-logo-white.png"  alt="White Appletree logo" />
       </a>
--- src/sections/PaymentMethods.jsx
+++ src/sections/PaymentMethods.jsx
@@ -11,22 +11,18 @@ const PaymentMethods = () => {
     const data = paymentMethodsOffered
 
   return (
-    <section id='payment-methods' className=' flex flex-col items-center pb-12' style={{ backgroundColor: colorPrimary }}>
+    <section id='payment-methods' className=' -z-10 flex flex-col items-center px-4 lg:px-12 pb-12' style={{ backgroundColor: colorPrimary }}>
       <h2 className=' text-xl md:text-2xl lg:text-3xl pt-12 font-bold'>Payment Methods</h2>
       
       <h3 style={{ color: textLight }} className=' mt-2 text-center text-lg md:text-2xl lg:text-3xl font-extralight'>Local and International Cards <br />Instant Bank Transfers, Mobile Money</h3>
     
-      <section className=' bg-red-600 payment-method-scroll mt-16 flex gap-y-4 overflow-scroll w-full gap-4 min-h-16 justify-between items-center'>
-        <div className=' bg-green-600 h-16 w-12'>l</div>
-        
-        {
-          data?.map(({image, title}) => (
-            <PaymentMethodCard image={image} title={title} />
-          ))
-        }
-
-        <div className=' bg-green-600 h-16 w-12'></div>
-      </section>
+        <section className=' w-full bg-red-600f overflow-scroll payment-method-scroll  mt-16 flex gap-y-4  gap-4 min-h-16 justify-between items-center'>
+          {
+            data?.map(({image, title}) => (
+              <PaymentMethodCard image={image} title={title} />
+            ))
+          }
+        </section>
     </section>
   )
 }

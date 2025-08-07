e1d6e27a8dd4bb507cb06b406920b57d62811595
khomabhena <colwanymab@gmail.com>
2025-08-07 17:33:50 +0200

    added privacy policy data and hero image

--- src/data/privacy-policy.js
+++ src/data/privacy-policy.js
@@ -82,7 +82,173 @@ export const getPrivacyPolicy = [
         titleNumber: '3.2',
         paragraphs: [
             {
-                para: 
+                para: 'When you make a payment by card on a Partnerï¿½s website, you must indicate your last name, first name and card data. In order to guarantee the highest level of security in processing payment data, this Data is never accessible to the Partner. Only Appletree Payments, has access to this Data in order to carry out the transaction. The Data is transmitted in an encrypted and secured manner in order to guarantee confidentiality. We inform you that Appletree Payments is PCI-DSS compliant.  The PCI-DSS standard serves as a reference for the technical and operational conditions for protecting the data of cardholders.'
+            }
+        ]
+    },
+    {
+        title: 'For what purposes is your Personal Data processed?',
+        titleNumber: '4.',
+        paragraphs: [
+            {
+                para: 'Your Personal Data is processed in the framework of performing the contract that you have entered into with Appletree Payments. This includes the following:',
+                points: [
+                    {
+                        point: 'Registering with the services and opening your account (payment account or electronic money account) on Appletree Paymentsï¿½ books;'
+                    },
+                    {
+                        point: 'Managing these accounts and carrying out payment operations;'
+                    },
+                    {
+                        point: 'Managing payment orders;'
+                    },
+                    {
+                        point: 'Managing client relations (for example, providing statements of operations);'
+                    },
+                    {
+                        point: 'Handling your questions and your potential claims;',
+                    },
+                    {
+                        point: 'The methods for online access of your account (and managing the authentication procedures) and'
+                    },
+                    {
+                        point: 'Making payments by card on the Partnerï¿½s site'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: 'Appletree Payments also processes your Personal Data in the framework of legitimate interests, deemed necessary to their activity as a provider of payment services. This includes the following:',
+        titleNumber: '',
+        paragraphs: [
+            {
+                para: 'The fight against identity fraud;',
+            },
+            {
+                para: 'The fight against external fraud;',
+            },
+            {
+                para: 'The fight against card payment fraud;',
+            },
+            {
+                para: 'The General Maintenance of the security of the Appletree Payments API and services.',
+            },
+            {
+                para: 'Finally, Appletree Payments manages your data for legal purposes. This includes but is not limited to:',
+                points: [
+                    {
+                        point: 'Respecting the legal and regulatory obligations imposed on Appletree Payments in their capacity as a provider of payment services, and specifically in terms of the fight against money laundering and the financing of terrorism;'
+                    },
+                    {
+                        point: 'Cooperating with public authorities and any authority in charge of applying the law or prudential supervision, in the event of oversight or inquiry.'
+                    }
+                ]
+            },
+        ],
+    },
+    {
+        title: 'How long is your Personal Data kept?',
+        titleNumber: '5.',
+        paragraphs: [
+            {
+                para: 'Appletree Payments will keep your Personal Data during the term of the contractual relationship. When you terminate using the services, all of your Personal Data will be definitively erased, with the exception of data that must be kept for legal reasons. The following time limits for keeping the data specifically apply:'
+            },
+            {
+                para: 'For reasons regarding the obligations in terms of the fight against money laundering and the financing of terrorism, Appletree Payments will keep documents and information regarding your identity for five years from the time services are terminated.  Documents and information regarding payment operations, as well as the documents indicating the characteristics of operations identified as particularly complex or pertaining to an unusually high amount or that do not seem to have economic justification or legal intents will also be kept for five years after termination of services.'
+            },
+            {
+                para: 'Data necessary for handling potential contestations or disputes will be kept for a time frame of five years, pursuant to the legal provisions in force (specifically but not exclusively those established in the Code of Commerce, the Civil Code and the Consumer Code).'
+            },
+            {
+                para: 'Contractual documents will be kept for a time frame of five years from the end of the contract.'
+            },
+            {
+                para: 'In the event of an objection to a means of payment, notification regarding the objection will be kept for a time frame of 18 months from the time of the objection.'
+            },
+            {
+                para: 'Communications with customer support may also be recorded. If that is the case, they will be kept for a time frame of five years from the time they are received or recorded.'
+            }
+        ]
+    },
+    {
+        title: 'Where is Personal Data stored?',
+        titleNumber: '6.',
+        paragraphs: [
+            {
+                para: 'The servers used by Appletree Payments to store your Personal Data are located in Zimbabwe.'
+            },
+            {
+                para: 'Furthermore, Appletree Payments will transmit some of your Data to its subcontractors and/or service providers necessary for carrying out the services. Some of these subcontractors store your Data on servers situated outside of the territory of Zimbabwe, particularly in the European Union. In this case, Appletree Payments ensures that this third-party country or entity in question has been subject to the decisions of the European Commission establishing an adequate level of protection of personal data (compliance with Privacy Shield). Otherwise, Appletree Payments will implement appropriate guarantees in order to ensure the protection of your Data.'
+            }
+        ]
+    },
+    {
+        title: 'Who are the recipients of Personal Data? Is it transferred to third parties?',
+        titleNumber: '7.'
+    },
+    {
+        title: 'Authorized Appletree Payments Services',
+        titleNumber: '7.1',
+        paragraphs: [
+            {
+                para: 'Only Appletree Paymentsï¿½ collaborators that are specifically authorised may access your Personal Data, in the framework of carrying out their missions. All collaborators of Appletree Payments having access to your Data are subject to strict confidentiality obligations, as well as professional secrecy concerning payment data.'
+            }
+        ]
+    },
+    {
+        title: 'The appropriate authorities',
+        titleNumber: '7.2',
+        paragraphs: [
+            {
+                para: 'Appletree Payments may be required to transfer Personal Data to the certain authorities, such as public authorities, organisations for the fight against money laundering and the financing of terrorism, or the authorities for banking oversight.',
+            }
+        ]
+    },
+    {
+        title: 'Subcontractors',
+        titleNumber: '7.3',
+        paragraphs: [
+            {
+                para: 'Appletree Payments uses the services of subcontractors to provide payment services and services that you request for example, hosting an information technology system, Partner credit institutions for protecting funds or carrying out payment operations, etc. The subcontractors process your Personal Data only on Appletree Paymentsï¿½ instruction and exclusively in the framework of the latterï¿½s activities. Barring express agreement on your part, the subcontractors are not authorised to access your Personal Data for their own use.'
+            },
+            {
+                para: 'Subcontractors are contractually obligated to comply with the obligations of security and confidentiality, to implement appropriate technical and organisational measures so that data processing is carried out in a manner that complies with applicable regulations and guarantees the protection of your rights. By a request sent to Appletree Payments, you may obtain a detailed list of the categories of subcontractors involved in processing your Data.'
+            }
+        ]
+    },
+    {
+        title: 'Your rights concerning your Personal Data',
+        titleNumber: '8.'
+    },
+    {
+        title: 'Right of access',
+        titleNumber: '8.1',
+        paragraphs: [
+            {
+                para: 'You have the right to access Data pertaining to you. If you exercise this right, Appletree Payments will send you a copy of the characteristics of the processing of your Data i.e. the purposes of data processing, the categories of Data in question, etc. This information will be provided to you in a currently used electronic format. However, you may request that this information be provided to you in another format, provided that Appletree Payments is technically capable of providing you the information in the format requested.'
+            },
+            {
+                para: 'You are informed that Appletree Payments may require the payment of fees based on the administrative costs incurred due to requests for additional copies.'
+            }
+        ]
+    },
+    {
+        title: 'Right of rectification',
+        titleNumber: '8.2',
+        paragraphs: [
+            {
+                para: 'If you are aware that the Data pertaining to you is inexact or incomplete, you have the right to request that this Data be rectified or updated.'
+            }
+        ]
+    },
+    {
+        title: 'Right to be forgotten',
+        titleNumber: '8.3',
+        paragraphs: [
+            {
+                para: 'In compliance with applicable regulations, you may request erasure of your Data in the following cases:',
+                
             }
         ]
     }
